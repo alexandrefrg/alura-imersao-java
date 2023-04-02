d1ab44d8202d1933950e6bacebc3cbc81946f118
Alexandre Garcia <alexandrefrg@hotmail.com>
2023-04-02 15:12:07 -0300

    fix: removing unused imports

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,9 +1,7 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public class App {
   public static void main(String[] args) throws Exception {
