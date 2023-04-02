c4e9c16f25777059e212e290ea9f9196d4923aa8
Alexandre Garcia <alexandrefrg@hotmail.com>
2023-04-02 14:11:07 -0300

    feature: finished class 3

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,12 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -14,52 +9,40 @@ public class App {
   public static void main(String[] args) throws Exception {
 
     // fazer uma conexão HTTP e buscar os top 250 filmes
+
     // Top 10 Movies
     // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+
     // Most popular videos
-    // String apiKey = System.getenv("IMDB_API_KEY");
-    // System.out.println(apiKey);
-    String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-    URI endereco = URI.create(url);
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-    String body = response.body();
+    // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+    // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-    // pegar só os dados que interessam (titulo, poster, classificação)
-    JsonParser parser = new JsonParser();
-    List<Map<String, String>> listaDeFilmes = parser.parse(body);
+    // Nasa
+    String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+    ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
+    var http = new ClienteHttp();
+    String json = http.buscaDados(url);
+    
     // exibir e manipular os dados
+    List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+    GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
 
     // verifica se o diretório de saída das figurinhas já existe
     File diretorio = new File("figurinhas/");
     diretorio.mkdir();
 
-    // cria instância da geradora de figurinhas
-    GeradoraDeFigurinhas geradora =  new GeradoraDeFigurinhas();
-    for (Map<String,String> filme : listaDeFilmes) {
+    for (int i = 0; i < 3; i++) {
 
-      String urlImagem = filme.get("image");
-      String titulo = filme.get("title");
-      Double classificacao = Double.parseDouble(filme.get("imDbRating"));
-      String nomeArquivo = "figurinhas/" + titulo.replaceAll(" ", "_") + ".png";
+      Conteudo conteudo = conteudos.get(i);
 
-      String textoFigurinha;
-      InputStream inputStreamImagemSobreposicao;
-      if (classificacao >= 8.0) {
-        textoFigurinha = "TOPZERA";
-        inputStreamImagemSobreposicao = new FileInputStream("pastaSobreposicao/thumbs_up.png");
-      } else {
-        textoFigurinha = "HMMMMM...";
-        inputStreamImagemSobreposicao = new FileInputStream("pastaSobreposicao/thumbs_down.png");
-      }
-      
-      InputStream inputStream = new URL(urlImagem).openStream();
+      String nomeArquivo = "figurinhas/" + conteudo.getTitulo().replaceAll(" ", "_") + ".png";
+      InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
 
-      geradora.cria(inputStream, nomeArquivo, textoFigurinha, inputStreamImagemSobreposicao);
-      
-      System.out.println("\u001b[1mTítulo:\u001b[m " + titulo);
+      geradora.cria(inputStream, nomeArquivo);
+
+      System.out.println("\u001b[1mTítulo:\u001b[m " + conteudo.getTitulo());
       System.out.println();
     }
   }
--- alura-stickers/src/GeradoraDeFigurinhas.java
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,12 +1,7 @@
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,12 +10,9 @@ import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
 
-  public void cria(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStreamSobreposicao) throws Exception {
+  public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
     // leitura da imagem
-    // InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"))
-    // InputStream inputStream = new
-    // URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
     BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
     // cria nova imagem em memória com transparência e com tamanho novo
@@ -28,15 +20,14 @@ public class GeradoraDeFigurinhas {
     int altura = imagemOriginal.getHeight();
     int alturaAdicional = 200;
     int novaAltura = altura + alturaAdicional;
+
     BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
-    
+
     // copiar imagem original pra nova imagem (em memória)
     Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
     graphics.drawImage(imagemOriginal, 0, 0, null);
-    
-    BufferedImage imageSobreposicao = ImageIO.read(inputStreamSobreposicao);
-    int posicaoImagemSobreposicaoY = novaAltura - imageSobreposicao.getHeight();
-    graphics.drawImage(imageSobreposicao, 0, posicaoImagemSobreposicaoY, null);
+
+    String texto = "TOPZERA";
 
     // configurar a fonte
     Font fonte = new Font("Impact", Font.BOLD, 80);
@@ -51,23 +42,9 @@ public class GeradoraDeFigurinhas {
     // definições do eixos X e Y do texto
     int posicaoTextoX = (largura - larguraFonte) / 2;
     int posicaoTextoY = novaAltura - 100;
+
     // desenha o texto
     graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
-    
-    FontRenderContext fontRenderContext = graphics.getFontRenderContext();
-    TextLayout textLayout = new TextLayout(texto, fonte,fontRenderContext);
-    
-    Shape outline = textLayout.getOutline(null);
-    AffineTransform transform = graphics.getTransform();
-    transform.translate(posicaoTextoX, posicaoTextoY);
-    graphics.setTransform(transform);
-
-    BasicStroke outlineStroke = new BasicStroke(largura * 0.004f);
-    graphics.setStroke(outlineStroke);
-
-    graphics.setColor(Color.BLACK);
-    graphics.draw(outline);
-    graphics.setClip(outline);
 
     // escrever a nova imagem em um arquivo
     ImageIO.write(novaImagem, "png", new File(nomeArquivo));
